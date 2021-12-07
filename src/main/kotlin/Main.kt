25fad8b59dfa51d3a2b125f84c800d6f526b1305
Polina Novikova <9104785163@mail.ru>
2021-12-08 00:33:33 +0300

    first version

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+    val transferAmount = 8000_00
+    val commissionPercentage = 0.0075
+    val minCommission = 3500
+
+    val commissionCalc = (transferAmount * commissionPercentage).toInt()
+    val commission = if (commissionCalc < minCommission) minCommission else commissionCalc
+    println("""Задача №1 - Денежные переводы
+============================================
+  Сумма перевода:  $transferAmount коп
+  Размер комиссии: $commission коп
+============================================
+    """.trimIndent())
+
+
 }
